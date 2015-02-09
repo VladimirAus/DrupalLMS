7472d844ced37cd40cd4d87b2cadb4bfb707467a
VladimirAus <v_rouakov@yahoo.com>
2015-02-10 01:47:41 +1000

    Update to install script

--- bin/lms.d8.install.with-config.sh
+++ bin/lms.d8.install.with-config.sh
@@ -24,7 +24,24 @@ mv drupal-$DVER docroot
 cd docroot
 
 drush7 si minimal --db-url="$DBCONN" --account-name=$DRUPLOGIN --account-pass=$DRUPPSWD --yes
-cp -r ../config/$LMSCONFIG sites/default/files
-mv sites/default/files/$LMSCONFIG sites/default/files/config_$LMSCONFIG
-echo "\$config_directories['$LMSCONFIG'] = 'sites/default/files/config_$LMSCONFIG';" >> sites/default/settings.php
-drush7 config-import $LMSCONFIG -y
+# cp -r ../config/$LMSCONFIG sites/default/files
+# mv sites/default/files/$LMSCONFIG sites/default/files/config_$LMSCONFIG
+# chmod -r 775 sites/default/files/config_$LMSCONFIG
+# echo "\$config_directories['$LMSCONFIG'] = 'sites/default/files/config_$LMSCONFIG';" >> sites/default/settings.php
+SITEUUID="uuid: $(drush7 config-get system.site uuid --format=string)"
+
+cd sites/default/files/
+CONFIGDIR="$(ls -d c* | head -1)"
+cd ../../../../config/$LMSCONFIG
+cp -R . ../../docroot/sites/default/files/$CONFIGDIR/staging/
+
+cd ../..
+sed -i.bak "1s/.*/$SITEUUID/" docroot/sites/default/files/$CONFIGDIR/staging/system.site.yml
+
+cd docroot/
+
+# Following commands do not work in core beta6
+# WORKAROUND: Login to website, enable config module, import staging
+# drush7 en config -y
+# drush7 config-import staging -y
+
