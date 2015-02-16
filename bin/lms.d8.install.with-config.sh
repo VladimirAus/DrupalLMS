8d733d810221dc58d4aece27a160b8741f6ec137
VladimirAus <v_roudakov@yahoo.com>
2015-02-16 23:28:00 +1000

    Updated install script

--- bin/lms.d8.install.with-config.sh
+++ bin/lms.d8.install.with-config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # General params
-readonly DVER="8.0.0-beta6"
+# readonly DVER="8.0.0-beta6"
+readonly DVER="8.0.x-dev"
 readonly SITENAME="lms.drupal8.loc" 
 readonly DBNAME="lms_drupal8_loc"
 
@@ -18,7 +19,7 @@ readonly DBCONN="mysql://$DBLOGIN:$DBPSWD@$DBSERVER/$DBNAME"
 readonly LMSCONFIG="lms.d8.config"
 
 # Download Drupal
-drush dl drupal-8
+drush dl drupal-8.0.x # install latest Drupal
 rm -rf docroot
 mv drupal-$DVER docroot
 cd docroot
@@ -36,13 +37,14 @@ CONFIGDIR="$(ls -d c* | head -1)"
 cd ../../../../config/$LMSCONFIG
 cp -R . ../../docroot/sites/default/files/$CONFIGDIR/staging/
 
-# cd ../..
-# sed -i.bak "1s/.*/$SITEUUID/" docroot/sites/default/files/$CONFIGDIR/staging/system.site.yml
+# Following commands do not work in core beta6 and earlier
+# Comment all commands and proceed to instaructons
 
-# cd docroot/
+cd ../..
+sed -i.bak "1s/.*/$SITEUUID/" docroot/sites/default/files/$CONFIGDIR/staging/system.site.yml
 
-# Following commands do not work in core beta6
-# WORKAROUND: Login to website, enable config module, import staging
-# drush en config -y
-# drush config-import staging -y
+cd docroot/
+
+drush en config -y
+drush config-import staging -y
 
