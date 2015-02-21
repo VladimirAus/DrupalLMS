293c9e613b236500b230a1a6a9b6e2728c38ab43
VladimirAus <v_roudakov@yahoo.com>
2015-02-21 12:42:33 +1000

    Yeoman angular distribution wih SASS

---
+++ frontang/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name d8frontendApp
+ * @description
+ * # d8frontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('d8frontendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
