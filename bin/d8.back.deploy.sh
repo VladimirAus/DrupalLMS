798289bc29ae36c2ea586d398498b39578c1ec6b
VladimirAus <v_roudakov@yahoo.com>
2015-02-23 00:34:09 +1000

    Updating backend script for no sude options

--- bin/d8.back.deploy.sh
+++ bin/d8.back.deploy.sh
@@ -20,14 +20,15 @@ readonly LMSCONFIG="lms.d8.config"
 
 # Download Drupal
 # drush dl drupal-8.0.x # install latest Drupal
-rm -rf docroot
+sudo rm -rf docroot
 # mv drupal-$DVER docroot
 drush make config/d8.lms.courses.make docroot --yes
 cd docroot
+chmod -R 777 sites/default/files
 
 # drush sql-create --db-url="$DBCONN" --yes
 drush si minimal --db-url="$DBCONN" --account-name=$DRUPLOGIN --account-pass=$DRUPPSWD --yes
-pause 'Press [Enter] key to continue...'
+chmod 755 sites/default/settings.php
 
 # cp -r ../config/$LMSCONFIG sites/default/files
 # mv sites/default/files/$LMSCONFIG sites/default/files/config_$LMSCONFIG
@@ -35,10 +36,11 @@ pause 'Press [Enter] key to continue...'
 # echo "\$config_directories['$LMSCONFIG'] = 'sites/default/files/config_$LMSCONFIG';" >> sites/default/settings.php
 SITEUUID="uuid: $(drush7 config-get system.site uuid --format=string)"
 
+drush config-export staging -y
 cd sites/default/files/
 CONFIGDIR="$(ls -d c* | head -1)"
 cd ../../../../config/$LMSCONFIG
-cp -R . ../../docroot/sites/default/files/$CONFIGDIR/staging/
+cp -Rf . ../../docroot/sites/default/files/$CONFIGDIR/staging/
 
 # Following commands do not work in core beta6 and earlier
 # Comment all commands and proceed to instaructons
@@ -48,11 +50,10 @@ cd ../..
 sed -i.bak "1s/.*/$SITEUUID/" docroot/sites/default/files/$CONFIGDIR/staging/system.site.yml
 
 cd docroot/
-chmod 766 sites/default/settings.php
 
 drush en config -y
-drush config-import staging -y
+# drush config-import staging -y
 
 # name is not set via config at the moment
-drush config-set system.site name 'Project Lantern' -y
+# drush config-set system.site name 'Project Lantern' -y
 
